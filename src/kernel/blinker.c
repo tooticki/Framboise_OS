fc8f35104ac82bc816bfa29534fe46cc308c5634
Guiloooo <guiloooo@mlir.info>
2018-05-03 22:59:58 +0200

    Cleaned up blinker

--- src/kernel/blinker.c
+++ src/kernel/blinker.c
@@ -1,9 +1,11 @@
 #include "blinker.h"
+#include "peripheral.h"
+#include "timer.h"
 
-void blinker(int n)
+void blinker(unsigned int n)
 {
     /* Assign the address of the GPIO peripheral (Using ARM Physical Address) */
-    gpio = (unsigned int*)L_GPIO_BASE;
+    volatile unsigned *gpio = (unsigned int*)L_GPIO_BASE;
 
     /* Write 1 to the GPIO16 init nibble in the Function Select 1 GPIO
        peripheral register to enable GPIO16 as an output */
@@ -12,19 +14,17 @@ void blinker(int n)
     /* Blink n times in a row*/
     for(int i = 0; i < n; i++)
     {
-        for(tim = 0; tim < 1200000; tim++)
-            ;
+      udelay(1000000);
 
-	/* Set the LED GPIO pin high ( Turn OK LED off for original Pi, and on
-           for plus models )*/
-        gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
+	/* Set the LED GPIO pin high (Turn OK LED off for original Pi, and on
+           for plus models) */
+      gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
 
-        for(tim = 0; tim < 1200000; tim++)
-            ;
+      udelay(1000000);
 
         /* Set the LED GPIO pin low ( Turn OK LED on for original Pi, and off
            for plus models )*/
-        gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);	
-    }
+      gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);	
+  }
 }
 
--- src/kernel/peripheral.h
+++ src/kernel/peripheral.h
@@ -1,16 +1,37 @@
+/* Magic numbers for accessing the hardware */
+
 #ifndef PERIPHERAL_H
 #define PERIPHERAL_H
 
 #define PERIPHERAL_BASE 0x20000000
-//#define PERIPHERAL_BASE  0x3F000000 // For 2
-
 #define PERIPHERAL_LENGTH 0x01000000
 
-#define SYSTEM_TIMER_OFFSET 0x3000 // to check
-#define INTERRUPTS_OFFSET 0xB000   // to check
+#define SYSTEM_TIMER_OFFSET 0x3000 // TODO: check me
+#define INTERRUPTS_OFFSET 0xB000   // TODO: check me
 
 #define MAILBOX_OFFSET 0xB880
 #define UART0_OFFSET 0x201000
 #define GPIO_OFFSET 0x200000
+
+#define GPIO_GPFSEL0    0
+#define GPIO_GPFSEL1    1
+#define GPIO_GPFSEL2    2
+#define GPIO_GPFSEL3    3
+#define GPIO_GPFSEL4    4
+#define GPIO_GPFSEL5    5
+
+#define GPIO_GPSET0     7
+#define GPIO_GPSET1     8
+
+#define GPIO_GPCLR0     10
+#define GPIO_GPCLR1     11
+
+#define L_GPIO_BASE     0x20200000UL
+#define LED_GPFSEL      GPIO_GPFSEL4
+#define LED_GPFBIT      21
+
+#define LED_GPSET       GPIO_GPSET1
+#define LED_GPCLR       GPIO_GPCLR1
+#define LED_GPIO_BIT    15
 
 #endif
